84c36d9a7fb3622cd47748d64e542084f1d37644
rinnnshinnn <rinnnshinnn@gmail.com>
2024-08-18 23:01:14 +0800

    Add getComputerChoice function
    - Implemented a function to randomly select "rock", "paper", or "scissors".
    - Utilized Math.random() and Math.round() to generate a random number.
    - Added conditional statements to return the corresponding choice based on the random number.
    - Included a default case to handle any unexpected values.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+console.log(getCOmputerChoice());
+
+function getCOmputerChoice() {
+    const choice = Math.round(Math.random() * 3);
+    
+    if (choice === 1) {
+        return "rock";
+    } else if (choice === 2) {
+        return "paper";
+    } else if (choice === 3) {
+        return "scissors";
+    } else {
+        return "Invalid choice!"; 
+    }
+}
